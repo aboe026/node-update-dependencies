3afd33f4fb8ea2f8e0f4bd0d5890cb869129caca
Adam Boe <adamboe@outlook.com>
2023-05-21 21:50:44 -0700

    double jest timeout

--- jest.config.e2e.ts
+++ jest.config.e2e.ts
@@ -4,7 +4,7 @@ import config from './jest.config'
 
 const e2eConfig: Config = {
   setupFilesAfterEnv: ['./test/e2e/util/_suite_setup_teardown.ts'],
-  testTimeout: 30000,
+  testTimeout: 60000,
 }
 
 export default {
